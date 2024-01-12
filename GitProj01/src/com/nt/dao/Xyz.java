3e8312b98c4e73b9eddf68a83668bb6c54d43a7d
chinu <chinu@INTSM21LP-24>
2024-01-12 20:49:27 +0530

    b1commit from dev1

--- GitProj01/src/com/nt/dao/Xyz.java
+++ GitProj01/src/com/nt/dao/Xyz.java
@@ -4,11 +4,13 @@ public class Xyz {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to Git proj");
-	}
 	
-	public void m1() {
-		Xyz xyz=new Xyz();
-		xyz.m1();
-	}
+
+	
+	F1 f1= new F1();
+	int result=f1.add(10, 20);
+	System.out.println("Result is"+result);
+	
+ }
 
 }
