50bbdcfcf742879fe03a2776a9ee45f2290aa968
chinu <chinu@INTSM21LP-24>
2024-01-12 15:50:23 +0530

    changes done by dev1

--- GitProj01/src/com/nt/dao/Test1.java
+++ GitProj01/src/com/nt/dao/Test1.java
@@ -5,5 +5,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Boss");
 	}
+	
+	public void myclass() {
+		System.out.println("Nice to meet you");
+	}
 
 }
